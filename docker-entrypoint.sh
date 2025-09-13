80669c50e5ea9fd2b855c755899ecb3caa107cec
SarahHerberichs <sarah.herberichs@neuf.fr>
2025-09-13 18:37:51 +0200

    fix: try to update db with migrationfile

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,17 +1,21 @@
 #!/bin/sh
 set -e
 
-# Attendre la DB
 echo "--- Attente de la base de données ---"
 until nc -z -v -w30 mysql.railway.internal 3306
 do
   echo "En attente de la connexion à la base de données..."
   sleep 1
 done
-echo "Base de données prête."
 
-# Lancer les migrations
+echo "Base de données prête. Démarrage des migrations..."
+
+# Variables d'environnement prod
+export APP_ENV=prod
+export APP_DEBUG=0
+
+# Exécuter les migrations sans chercher de .env
 php bin/console doctrine:migrations:migrate --no-interaction --env=prod
 
-# Démarrer PHP-FPM
-exec "$@"
+# Lancer le serveur ou le processus principal
+php-fpm
