2422fd150c272edb68fd872899b39f0718af62a5
SarahHerberichs <sarah.herberichs@neuf.fr>
2025-09-15 14:09:53 +0200

    fix:migrations problem 11

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -18,6 +18,11 @@ touch /var/www/html/var/log/php_errors.log
 # Start the PHP-FPM process in the background
 exec "$@"
 
+# Créer le fichier de log pour éviter que tail ne plante
+touch /var/www/html/var/log/php_errors.log
 
-# Tail the log file to see all errors
+# Démarrer le processus PHP-FPM en arrière-plan
+php-fpm -F -d "listen=0.0.0.0:$PORT" &
+
+# Afficher les logs en continu pour le débogage
 tail -f /var/www/html/var/log/php_errors.log
