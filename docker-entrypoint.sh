6167da98b43f79fa3879eb44415f3f807f819f14
SarahHerberichs <sarah.herberichs@neuf.fr>
2025-09-15 15:30:43 +0200

    fix: nelmio cors

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -8,7 +8,5 @@ echo "Running migrations..."
 php bin/console doctrine:migrations:migrate --no-interaction
 echo "Migrations done."
 
-# This part is crucial for debugging
-echo "Starting PHP-FPM in foreground with error logging..."
-# Ensure PHP-FPM logs to stdout so Railway can capture it
-exec php-fpm -F -O -y /usr/local/etc/php-fpm.conf
+# Démarrer PHP-FPM et Nginx ensemble
+php-fpm -D && nginx -g 'daemon off;'
