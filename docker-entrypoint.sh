f62272210ea12e7b00d3bb4ed59f7d70e8987177
SarahHerberichs <sarah.herberichs@neuf.fr>
2025-09-15 15:53:14 +0200

    fix:migrations problem 8

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -4,9 +4,8 @@ set -e
 echo "Waiting for the database..."
 /usr/local/bin/dockerize -wait tcp://mysql:3306 -timeout 20s
 
-echo "Running migrations..."
-php bin/console doctrine:migrations:migrate --no-interaction
-echo "Migrations done."
+# echo "Running migrations..."
+# php bin/console doctrine:migrations:migrate --no-interaction
+# echo "Migrations done."
 
-# Démarrer PHP-FPM et Nginx ensemble
 php-fpm -D && nginx -g 'daemon off;'
